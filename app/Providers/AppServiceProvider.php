48bd64decbfd961debdc57f54beff68fa0cd6680
RitaChan0401 <terminus_rita@yahoo.co.jp>
2019-06-29 18:36:18 +0900

    edit AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
     }
 }
